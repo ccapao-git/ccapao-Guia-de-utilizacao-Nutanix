74a9a02b0c98c368867baf477f48626a1207c186
ccapao-git <celso.capao@gmail.com>
2018-04-04 08:08:03 +0100

    Update workwithpetdb.py

--- workwithpetdb.py
+++ workwithpetdb.py
@@ -2,16 +2,14 @@
 
 from pymysql import connect, err, sys, cursors
 from json2html import *
-
+import socket
 import cgitb
 cgitb.enable()
 
-print "Content-type: text/html"
-print
-print "<html><head>"
-print ""
-print "</head><body>"
-print "Here you have:<br><br>"
+if socket.gethostname().find('.')>=0:
+    srvhostname=socket.gethostname()
+else:
+    srvhostname=socket.gethostbyaddr(socket.gethostname())[0]
 
 conn = connect( host = 'MYHOST',
                         port = MYPORT,
@@ -24,5 +22,12 @@ cursor = conn.cursor( cursors.DictCursor );
 cursor.execute( "SELECT * FROM pet" )
 data = cursor.fetchall()
 
+print "Content-type: text/html"
+print
+print "<html><head>"
+print ""
+print "</head><body>"
+print "  Web Server: " + srvhostname + "<br>"
+print "  DB Server: " + conn.host + ":" + str(conn.port) + "<br><br>"
 print(json2html.convert(json = data))
 print "</body></html>"
