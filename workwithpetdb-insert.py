be8fa8ed54b0c760d63c6f06a768090cda83bf48
ccapao-git <celso.capao@gmail.com>
2018-06-04 09:42:25 +0100

    Create workwithpetdb-insert.py

---
+++ workwithpetdb-insert.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+from pymysql import connect, err, sys, cursors
+from json2html import *
+import socket
+import cgitb
+import cgi
+#cgitb.enable()
+
+print 'Content-type: text/html'
+print
+print '<html><head>'
+print '<title>MYAPP</title>'
+print '</head><body>'
+
+conn = connect( host = 'MYDB',
+                        port = 3306,
+                        user = 'MYUSER',
+                        passwd = 'MYPASS',
+                        db = 'MYDB' );
+
+cursor = conn.cursor( cursors.DictCursor );
+
+# Create instance of FieldStorage
+form = cgi.FieldStorage()
+
+# Get data from fields
+name = form.getvalue('name')
+owner = form.getvalue('owner')
+species = form.getvalue('species')
+sex = form.getvalue('sex')
+birth = form.getvalue('birth')
+death = form.getvalue('death')
+
+cursor.execute("INSERT INTO pet VALUES (%s, %s, %s, %s, %s, %s)",[name,owner,species,sex,birth,death])
+conn.commit()
+
+print '<input type="button" value="Reload Page" onClick="location.href=index.py">'
+print '</body></html>'
